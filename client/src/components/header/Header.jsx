e2dab6a1c80b121051b848ebe896d9159127b452
Christian Marquez <christianmarquez41@gmail.com>
2021-08-26 13:52:21 -0400

    created header component and added some styling as well as icons

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,16 @@
-import "./header.css"
+import './header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton';
 
 export default function Header() {
-    return (
-        <div className="header">
-            HEADER
-        </div>
-    )
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header-icon" />
+      </IconButton>
+      <img className="header-logo" src="./assets/tinder-logo.png" alt="" />
+
+    </div>
+  );
 }
