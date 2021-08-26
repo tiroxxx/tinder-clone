9edad11c7664e4b57dadc92f94e7b29353140674
Christian Marquez <christianmarquez41@gmail.com>
2021-08-26 13:13:56 -0400

    first commit -m

---
+++ tinder-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* header */}
+      {/* tindercards */}
+      {/* swipebuttons */}
+    </div>
+  );
+}
+
+export default App;
